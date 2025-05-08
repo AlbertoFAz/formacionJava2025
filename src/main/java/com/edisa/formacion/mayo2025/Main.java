ad1f31f045ba9665f0294dbab76f69ecca53df86
Alberto Fernández Azcoaga <124345225+AlbertoFAz@users.noreply.github.com>
2025-05-08 10:35:01 +0200

    Ejercicio2

--- src/main/java/com/edisa/formacion/mayo2025/Main.java
+++ src/main/java/com/edisa/formacion/mayo2025/Main.java
@@ -11,22 +11,29 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Uso: java Main \"Texto para QR\" \"ruta\\al\\archivo.jpg\"");
+        if (args.length < 3) {
+            System.out.println("Uso: java Main \"Texto\" \"ruta\\salida.jpg\" \"Formato (QR_CODE, EAN_13, CODE_128...)\"");
             return;
         }
 
-        String textoQR = args[0];
-        String rutaSalida = args[1];
+        String textQR = args[0];
+        String finalPath = args[1];
+        String textFormat = args[2].toUpperCase();
 
-        int ancho = 300;
-        int alto = 300;
+        int width = 300;
+        int heigh = 300;
 
         try {
-            BitMatrix matrix = new MultiFormatWriter().encode(textoQR, BarcodeFormat.QR_CODE, ancho, alto);
-            Path path = FileSystems.getDefault().getPath(rutaSalida);
+            BarcodeFormat format = BarcodeFormat.valueOf(textFormat);
+            BitMatrix matrix = new MultiFormatWriter().encode(textQR, format, width, heigh);
+            Path path = FileSystems.getDefault().getPath(finalPath);
             MatrixToImageWriter.writeToPath(matrix, "jpg", path);
-            System.out.println("Código QR generado correctamente en: " + rutaSalida);
+            System.out.println("Código generado en formato " + format + " correctamente en: " + finalPath);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Formato no válido. Usa uno de los siguientes:");
+            for (BarcodeFormat bf : BarcodeFormat.values()) {
+                System.out.println("- " + bf.name());
+            }
         } catch (Exception e) {
             System.err.println("Error al generar el código QR: " + e.getMessage());
         }
